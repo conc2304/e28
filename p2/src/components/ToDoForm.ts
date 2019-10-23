9a9122dd4c4d3b59ac2686f2301c8c4e6bb8de39
clyzby <jose.conchello@gmail.com>
2019-10-23 17:11:36 -0400

    p2 end tutorial

--- p2/src/components/ToDoForm.ts
+++ p2/src/components/ToDoForm.ts
@@ -1,9 +1,13 @@
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
 import WithRender from './to-do-form.html';
+
 
 @WithRender
 @Component
 export default class ToDoForm extends Vue {
+
+    @Prop({type: String, default: 'Add Task'}) readonly buttonText!: string
+
     public task: string = '';
 
     public emitTask(): void {
