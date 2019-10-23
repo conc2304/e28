737cd32708a2b20a675d9b402f345052afdd142c
clyzby <jose.conchello@gmail.com>
2019-10-23 17:51:31 -0400

    p2 intial commit WIP

---
+++ p2/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import GuessedLetters from './components/GuessedLetters'
+import HangmanGallows from './components/HangmanGallows'
+import LettersToGuess from './components/LettersToGuess'
+import RestartGame from './components/RestartGame'
+import StrikesLeftCounter from './components/StrikesLeftCounter'
+import WordGuessForm from './components/WordGuessForm'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    HangManGallows,
+    StrikesLeftCounter,
+    GuessedLetters,
+    LettersToGuess,
+    GuessForm,
+    GameRestarter,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ p2/src/components/GuessedLetters.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="guessed-letters-wrapper">
+        <h3>Guessed Letters</h3>
+        <div v-for="(guess, index) in lettersGuessed" :key="lettersGuessed.id">{{ guess }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GuessedLetters',
+    data () {
+        return {}
+    },
+    props : {
+      lettersGuessed: {
+        type: Array,
+        defualt: []
+      }
+    }, 
+}
+</script>
