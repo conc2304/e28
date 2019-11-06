c7b6bfbcad44c766f697f7e636171427b76d98d9
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-11-05 20:54:46 -0500

    zipfoods ShowProducts / ShowProduct components

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,16 +1,22 @@
-<template>
-  <div id="app">
-    <img id="logo" src="./assets/images/zipfoods-logo.png" />
-    <p>{{ headline }}</p>
-  </div>
+<template lang="pug">
+  #app
+    img#logo(src="./assets/images/zipfoods-logo.png")
+    p {{ headline }}
+    ShowProducts
 </template>
 
 <script>
+import ShowProducts from './components/ShowProducts.vue';
+
 export default {
   name: 'App',
   data: () => ({
-    headline: 'ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.',
+    headline:
+      'ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.',
   }),
+  components: {
+    ShowProducts,
+  },
 };
 </script>
 <style lang="scss">
