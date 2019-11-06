f15f04993652c04ea2c829d893be251b962fa7e1
clyzby <jose.conchello@gmail.com>
2019-11-06 11:48:33 -0500

    zipfoods add routes

--- zipfoods/src/router/index.js
+++ zipfoods/src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import ShowHome from '../components/ShowHome.vue';
+import ShowProducts from '../components/ShowProducts.vue';
+import ShowCategories from '../components/ShowCategories.vue';
+
 Vue.use(VueRouter);
 
 const routes = [
-
+  { path: '/', component: ShowHome },
+  { path: '/products', component: ShowProducts },
+  { path: '/categories', component: ShowCategories },
 ];
 
 const router = new VueRouter({
