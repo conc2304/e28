b807a74dbe546f3f4145e008670e766b2eb17ed7
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-11-05 20:21:29 -0500

    zipfoods - setup app

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,32 +1,18 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <img id="logo" src="./assets/images/zipfoods-logo.png" />
+    <p>{{ headline }}</p>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    headline: 'ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.',
+  }),
+};
+</script>
+<style lang="scss">
+@import './assets/css/zipfoods.scss';
 </style>
